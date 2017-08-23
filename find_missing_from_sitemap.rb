8624df4ca4827d403c29c84a9fc9297cb3effc9a
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-08-23 14:41:47 +0000

    Ignore document duplicates with query strings

--- find_missing_from_sitemap.rb
+++ find_missing_from_sitemap.rb
@@ -3,14 +3,23 @@ require_relative "elasticsearch_client"
 
 input = [
   "/government/publications/designated-teacher-for-looked-after-children", # statutory_guidance
+  "/government/publications/designated-teacher-for-looked-after-children?some-query", # duplicate
   "/if-your-child-is-taken-into-care", # guidance
   "/government/news/government-to-extend-green-flag-award-for-5-more-years", # press_release
   "/not/a/govuk/path",
+  "/not/a/govuk/path?other-version",
 ]
 
 client = ElasticsearchClient.new
 
+missing_docs = Set.new
+
 input.each do |path|
-  es_doc = client.get(id: path)
-  puts es_doc
+  cleaned_path = path.sub(/\?.*$/, "")
+  es_doc = client.get(id: cleaned_path)
+
+  missing_docs << cleaned_path if es_doc.nil?
 end
+
+puts "#{missing_docs.count} missing documents"
+puts missing_docs.to_a
