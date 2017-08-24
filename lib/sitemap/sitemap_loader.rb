a654ddc99192bece883a78e9bd49a67086dbd77b
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-08-24 12:36:58 +0000

    Save an entire sitemap index to ES

--- lib/sitemap/sitemap_loader.rb
+++ lib/sitemap/sitemap_loader.rb
@@ -1,12 +1,25 @@
 class SitemapLoader
-  def self.iterator(sitemap_filename)
+  def self.iterator(sitemap_index)
     Enumerator.new do |yielder|
-      doc = File.open(sitemap_filename) { |f| Nokogiri::XML(f) }
-      locations = doc.css("url loc")
+      parent_doc = File.open(sitemap_index) { |f| Nokogiri::XML(f) }
+      directory = File.dirname(sitemap_index)
+      files = parent_doc.css("sitemap loc").map { |location| extract_filename(directory, location) }
 
-      locations.each do |loc|
-        yielder << loc.text
+      puts "Found #{files.count} files"
+      puts files
+
+      files.each do |file|
+        doc = File.open(file) { |f| Nokogiri::XML(f) }
+        locations = doc.css("url loc")
+
+        locations.each do |loc|
+          yielder << loc.text
+        end
       end
     end
+  end
+
+  def self.extract_filename(directory, location)
+    directory + "/" + location.text.match(/[^\/]*$/)[0]
   end
 end
