b4c45a8f139a8a37259034c328c8f9172c3f0c58
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-08-24 13:44:45 +0100

    Store the path rather than full URL from sitemap

--- lib/sitemap/sitemap_saver.rb
+++ lib/sitemap/sitemap_saver.rb
@@ -15,17 +15,26 @@ class SitemapSaver
     sitemap = SitemapLoader.iterator(sitemap_file)
 
     sitemap.each_with_index do |url, index|
+      path = extract_path(url)
+
       body = {
-        url: url
+        path: path
       }
       client.index(
         index: full_name,
         type: "sitemap_url",
-        id: url,
+        id: path,
         body: body
       )
 
       puts "Saved #{index} URLs" if index % 1000 == 0
     end
+  end
+
+private
+  def self.extract_path(url)
+    url.sub("https://www.gov.uk", "")
+      .sub("http://www.gov.uk", "")
+      .sub(/\?.*$/, "")
   end
 end
